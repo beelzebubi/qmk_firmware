fd8a8cae539fd302e01473981714087ffc8ff22b
Tom Sennewald <tom.sennewald@gmx.de>
2021-11-01 15:25:26 +0100

    Revert previous changes to default keymap

--- keyboards/lily58/keymaps/lily58l/rules.mk
+++ keyboards/lily58/keymaps/lily58l/rules.mk
@@ -1,12 +1,1 @@
 EXTRAKEY_ENABLE = yes
-
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
-LTO_ENABLE = yes 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
-
-WPM_ENABLE = yes
-
-EXTRAFLAGS += -flto
-MOUSEKEY_ENABLE = no
