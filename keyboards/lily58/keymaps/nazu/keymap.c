6c9f6b7715881c972d86b63d5d8b1847362f29a7
unknown <you@example.com>
2021-11-11 23:31:20 +0100

    Mended Colemak layout

--- keyboards/lily58/keymaps/nazu/keymap.c
+++ keyboards/lily58/keymaps/nazu/keymap.c
@@ -25,7 +25,6 @@
 extern uint8_t is_master;
 
 enum layers {
-//    _QWERTY,
     _QWERTZ,
     _COLEMAK,
     _NEO,
@@ -78,10 +77,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTZ] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    DE_SS,
-  KC_TAB,   DE_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     DE_Z,   KC_U,    KC_I,    KC_O,    KC_P,    DE_UDIA,
-  KC_LCTRL, DE_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,   KC_J,    KC_K,    KC_L,    DE_ODIA, DE_ADIA,
-  KC_LSFT,  DE_Y,   KC_X,    KC_C,    KC_V,    KC_B,  DE_HASH,  DE_PLUS, KC_N,   KC_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT,
+  KC_ESC,   DE_1,   DE_2,    DE_3,    DE_4,    DE_5,                     DE_6,   DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,
+  KC_TAB,   DE_Q,   DE_W,    DE_E,    DE_R,    DE_T,                     DE_Z,   DE_U,    DE_I,    DE_O,    DE_P,    DE_UDIA,
+  KC_LCTRL, DE_A,   DE_S,    DE_D,    DE_F,    DE_G,                     DE_H,   DE_J,    DE_K,    DE_L,    DE_ODIA, DE_ADIA,
+  KC_LSFT,  DE_Y,   DE_X,    DE_C,    DE_V,    DE_B,  DE_HASH,  DE_PLUS, DE_N,   DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT,
                              KC_LALT, KC_LGUI, LOWER, KC_ENT,   KC_SPC,  RAISE,  KC_BSPC, KC_RALT
 ),
 /*
@@ -102,9 +101,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_COLEMAK] = LAYOUT(
   KC_ESC,  CM_1,   CM_2,    CM_3,    CM_4,     CM_5,                    CM_6,    CM_7,    CM_8,    CM_9,    CM_0,    CM_MINS,
-  KC_TAB,  CM_Q,   CM_W,    CM_F,    CM_P,     CM_B,                    CM_J,    CM_L,    CM_U,    CM_Y,    CM_SCLN, CM_BSLS,
-  KC_LCTL, CM_A,   CM_R,    CM_S,    CM_T,     CM_G,                    CM_M,    CM_N,    CM_E,    CM_I,    CM_O,    CM_QUOT,
-  KC_LSFT, CM_Z,   CM_X,    CM_C,    CM_D,     CM_B,  CM_LBRC, CM_RBRC, CM_K,    CM_H,    CM_COMM, CM_DOT,  CM_SLSH, KC_RSFT,
+  KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_P,     KC_B,                    KC_J,    KC_L,    KC_U,    KC_Y,    CM_SCLN, CM_BSLS,
+  KC_LCTL, KC_A,   KC_R,    KC_S,    KC_T,     KC_G,                    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    CM_QUOT,
+  KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_D,     KC_V,  CM_LBRC, CM_RBRC, KC_K,    KC_H,    CM_COMM, CM_DOT,  CM_SLSH, KC_RSFT,
                             KC_LALT, KC_LCTRL, LOWER, KC_ENT,  KC_SPC,  RAISE,   KC_BSPC, KC_RALT
 ),
 /*
@@ -842,11 +841,6 @@ void oled_task_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-      /*case KC_QWERTY:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY);
-            }
-            return false; */
         case KC_QWERTZ:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_QWERTZ);
