bb4995247e08b445f5c6cd6102aa6759a508ed6a
tomse <tom.sennewald@gmx.de>
2022-05-04 18:45:03 +0200

    Added layout for macOS
    swapped Super and Alt

--- keyboards/kbdfans/kbd75/keymaps/nazu/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/nazu/keymap.c
@@ -5,12 +5,14 @@
 enum layers {
   _QWERTZ,
   _COLEMAK,
+  _MACOS,
   _FN1,
 };
 
 enum custom_keycodes {
     KC_QWTZ = SAFE_RANGE,     // change to QWERTZ layer
     KC_CLMK,                 // change to COLEMAK layer
+    KC_MAC,                 // change to MACOS layer
 };
 
 enum {
@@ -34,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------------|
    * | Shift | < > |  Y  |  X  |  C  |  V  |  B  |  N  |  M  | , ; | . : | - _ |  Shift  | Up  | End |
    * |-----------------------------------------------------------------------------------------------|
-   * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt | FN1 | App |Left |Down |Right|
+   * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt | FN1 |RCtl |Left |Down |Right|
    * `-----------------------------------------------------------------------------------------------´
    */
 	[_QWERTZ] = LAYOUT_iso_1u(
@@ -59,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------------|
    * | Shift |  Z  |  X  |  C  |  D  |  V  | \ | |  K  |  H  | , < | . > | / ? |  Shift  | Up  | End |
    * |-----------------------------------------------------------------------------------------------|
-   * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt | FN1 | App |Left |Down |Right|
+   * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt | FN1 |RCtl |Left |Down |Right|
    * `-----------------------------------------------------------------------------------------------´
    */
 	[_COLEMAK] = LAYOUT_iso_1u(
@@ -71,8 +73,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(_FN1), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
 ),
 
+  /*
+   * QWERTZ
+   * ,-----------------------------------------------------------------------------------------------,
+   * | Esc | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |Print|Pause| Del |
+   * ,-----------------------------------------------------------------------------------------------|
+   * |  ^  | 1 ! | 2 " | 3 § | 4 $ | 5 % | 6 & | 7 / | 8 ( | 9 ) | 0 = | ß ? | ´ ` | Backspace |Home |
+   * |-----------------------------------------------------------------------------------------------|
+   * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Z  |  U  |  I  |  O  |  P  |  Ü  | + * | Enter  |PgUp |
+   * |----------------------------------------------------------------------------------       |-----|
+   * |   Caps  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  Ö  |  Ä  | # ' |       |PgDwn|
+   * |-----------------------------------------------------------------------------------------------|
+   * | Shift | < > |  Y  |  X  |  C  |  V  |  B  |  N  |  M  | , ; | . : | - _ |  Shift  | Up  | End |
+   * |-----------------------------------------------------------------------------------------------|
+   * | LCtl  | LAlt  | LGUI  |            Space                  |RGUI | FN1 |RAlt |Left |Down |Right|
+   * `-----------------------------------------------------------------------------------------------´
+   */
+	[_MACOS] = LAYOUT_iso_1u(
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_PAUS,  KC_DEL,
+    DE_CIRC,  DE_1,     DE_2,     DE_3,     DE_4,     DE_5,     DE_6,     DE_7,     DE_8,     DE_9,     DE_0,     DE_SS,    DE_ACUT,            KC_BSPC,  KC_HOME,
+    KC_TAB,   DE_Q,     DE_W,     DE_E,     DE_R,     DE_T,     DE_Z,     DE_U,     DE_I,     DE_O,     DE_P,     DE_UDIA,  DE_PLUS,            KC_ENT,   KC_PGUP,
+    KC_CAPS,  DE_A,     DE_S,     DE_D,     DE_F,     DE_G,     DE_H,     DE_J,     DE_K,     DE_L,     DE_ODIA,  DE_ADIA,  KC_BSLS,                      KC_PGDN,
+    KC_LSFT,  DE_LABK,  DE_Y,     DE_X,     DE_C,     DE_V,     DE_B,     DE_N,     DE_M,     DE_COMM,  DE_DOT,   DE_MINS,  KC_RSFT,            KC_UP,    KC_END,
+    KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RGUI,  MO(_FN1), KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT
+  ),
+
 	[_FN1] = LAYOUT_iso_1u(
-    RESET,    KC_QWTZ,  KC_CLMK,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MUTE,
+    RESET,    KC_QWTZ,  KC_CLMK,  KC_MAC,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MUTE,
     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            _______,  XXXXXXX,
     _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,
     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                      XXXXXXX,  XXXXXXX,
@@ -91,6 +118,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_CLMK:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_COLEMAK);
+            }
+            return false;
+        case KC_MAC:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_MACOS);
             }
             return false;
     }
