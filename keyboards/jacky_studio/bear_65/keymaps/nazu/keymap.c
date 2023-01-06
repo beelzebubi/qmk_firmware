ce5eaa8ca787d5401cf5887e217d2cd761c17137
Tom Sennewald <tom@sennewald.me>
2023-01-06 11:25:38 +0100

    streamlined keeb layouts

--- keyboards/jacky_studio/bear_65/keymaps/nazu/keymap.c
+++ keyboards/jacky_studio/bear_65/keymaps/nazu/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_FN1] = LAYOUT_wk(
              QK_BOOT, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           XXXXXXX, XXXXXXX,
-            KC_SWAP,  _______, QWERTY,  CLMKDH,  XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            KC_SWAP,  _______, QWERTY,  COLEMAK, CLMKDH,  XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
            KC_NORM,   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
                       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_VOLU,
                       _______, _______, _______,     KC_MPLY,      XXXXXXX,                      KC_MUTE,          _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT
--- keyboards/mode/m65ha_alpha/keymaps/nazu/keymap.c
+++ keyboards/mode/m65ha_alpha/keymaps/nazu/keymap.c
@@ -26,7 +26,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   /*
-   * Colemak
+   * Colemak DH
+   * ,-----------------------------------------------------------------------------------------------,
+   * |  `  | 1 ! | 2 @ | 3 # | 4 $ | 5 % | 6 ^ | 7 & | 8 * | 9 ( | 0 ) | - _ | = + | Backspace | Del |
+   * |-----------------------------------------------------------------------------------------------|
+   * |  Tab   |  Q  |  W  |  F  |  P  |  G  |  J  |  L  |  U  |  Y  | ; : | [ { | ] } |   \ |  |PgUp |
+   * |-----------------------------------------------------------------------------------------|-----|
+   * |Backspace|  A  |  R  |  S  |  T  |  D  |  H  |  N  |  E  |  I  |  O  | ' " |    Enter    |PgDwn|
+   * |-----------------------------------------------------------------------------------------------|
+   * |    Shift    |  Z  |  X  |  C  |  V  |  B  |  K  |  M  | , < | . > | / ? |  Shift  | Up  | End |
+   * |-----------------------------------------------------------------------------------------------|
+   * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt | FN1 |     |Left |Down |Right|
+   * `-----------------------------------------------------------------------------------------------´
+   */
+	[_COLEMAK] = LAYOUT_65_ansi_blocker(
+    KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,             KC_BSPC, KC_DEL,
+    KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,  KC_BSLS,           KC_PGUP,
+    KC_BSPC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,           KC_ENT,            KC_PGDN,
+    TD_CAPS,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,            KC_UP,   KC_END,
+    KC_LCTL,  KC_LGUI, KC_LALT,                                KC_SPC,                                  KC_RALT, MO(_FN1), KC_LEFT,  KC_DOWN, KC_RGHT
+),
+
+  /*
+   * Colemak DH
    * ,-----------------------------------------------------------------------------------------------,
    * |  `  | 1 ! | 2 @ | 3 # | 4 $ | 5 % | 6 ^ | 7 & | 8 * | 9 ( | 0 ) | - _ | = + | Backspace | Del |
    * |-----------------------------------------------------------------------------------------------|
@@ -47,11 +69,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,  KC_LGUI, KC_LALT,                                KC_SPC,                                  KC_RALT, MO(_FN1), KC_LEFT,  KC_DOWN, KC_RGHT
 ),
 
+  /*
+   * FN1
+   * ,-----------------------------------------------------------------------------------------------,
+   * |  `  | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |   Reset   |Mute |
+   * |-----------------------------------------------------------------------------------------------|
+   * |        |QWRTY|CLMAK|CLMKD|     |     |     |     |     |     |     |     |     |        |SWAP |
+   * |-----------------------------------------------------------------------------------------|-----|
+   * |         |     |     |     |     |     |     |     |     |     |     |     |             |NORM |
+   * |-----------------------------------------------------------------------------------------------|
+   * |             |     |     |     |     |     |     |     |     |     |     |         |VolU |Home |
+   * |-----------------------------------------------------------------------------------------------|
+   * |       |       |       |           Play/Pause              |     |     |     |Prev |VolD |Next |
+   * `-----------------------------------------------------------------------------------------------´
+   */
 	[_FN1] = LAYOUT_65_ansi_blocker(
     KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           QK_BOOT, KC_MUTE,
-    _______,  QWERTY,  CLMKDH,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_SWAP,
+    _______,  QWERTY,  COLEMAK, CLMKDH,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_SWAP,
     _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,          KC_NORM,
-    _______,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          KC_VOLU, XXXXXXX,
+    _______,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          KC_VOLU, KC_HOME,
     _______,  _______,  _______,                                KC_MPLY,                                _______, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT
   ),
 
