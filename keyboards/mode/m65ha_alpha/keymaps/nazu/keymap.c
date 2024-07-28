6b70ca0bec24fcb3a15da10b968239ac2cda0274
beelzebubi <tom@sennewald.me>
2024-07-28 17:40:53 +0200

    added space fn

--- keyboards/mode/m65ha_alpha/keymaps/nazu/keymap.c
+++ keyboards/mode/m65ha_alpha/keymaps/nazu/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     KC_HYPR,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,             KC_PGDN,
     TD_CAPS,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    KC_LCTL,  KC_LGUI, KC_LALT,                                TD_SPC,                                  KC_RALT,  MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  KC_LGUI, KC_LALT,                           LT(_FN1, KC_SPC),                             KC_RALT,  MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   /*
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,  KC_BSLS,           KC_PGUP,
     KC_BSPC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,           KC_ENT,            KC_PGDN,
     TD_CAPS,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,            KC_UP,   KC_END,
-    KC_LCTL,  KC_LGUI, KC_LALT,                                TD_SPC,                                  KC_RALT, MO(_FN1), KC_LEFT,  KC_DOWN, KC_RGHT
+    KC_LCTL,  KC_LGUI, KC_LALT,                           LT(_FN1, KC_SPC),                             KC_RALT, MO(_FN1), KC_LEFT,  KC_DOWN, KC_RGHT
 ),
 
   /*
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,  KC_BSLS,           KC_PGUP,
     KC_BSPC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,           KC_ENT,            KC_PGDN,
     TD_CAPS,           KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,            KC_UP,   KC_END,
-    KC_LCTL,  KC_LGUI, KC_LALT,                                TD_SPC,                                  KC_RALT, MO(_FN1), KC_LEFT,  KC_DOWN, KC_RGHT
+    KC_LCTL,  KC_LGUI, KC_LALT,                           LT(_FN1, KC_SPC),                             KC_RALT, MO(_FN1), KC_LEFT,  KC_DOWN, KC_RGHT
 ),
 
   /*
@@ -100,10 +100,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 	[_FN1] = LAYOUT_65_ansi_blocker(
     KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           QK_BOOT, KC_MUTE,
-    _______,  QWERTY,  COLEMAK, CLMKDH,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_SWAP,
-    _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,          KC_NORM,
-    _______,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          KC_VOLU, KC_HOME,
-    _______,  _______,  _______,                                KC_MPLY,                                _______, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT
+    _______,  QWERTY,  COLEMAK, CLMKDH,  XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_SWAP,
+    _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,          XXXXXXX,          KC_NORM,
+    _______,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          KC_VOLU, KC_MPLY,
+    _______,  _______,  _______,                                KC_TRNS,                                _______, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT
   ),
 
 };
